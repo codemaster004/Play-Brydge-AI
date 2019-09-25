0e4ae69135584fa318b1fac157034f89b51e1649
codemaster004 <50377268+codemaster004@users.noreply.github.com>
2019-09-25 10:01:19 +0200

    Initial Commit

---
+++ Play Brydge AI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Play Brydge AI
+//
+//  Created by Filip on 25/09/2019.
+//  Copyright © 2019 Filip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
